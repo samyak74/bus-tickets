f82ce98f6cd7f1b8f804f5a024990350dfb7b30e
uday97 <uday.agg97@gmail.com>
2020-03-28 13:45:42 +0530

    added form page for login and signup, created user object middleware

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
+var User = require('./user');
 
 
 // GET route for reading data
 router.get('/', function (req, res, next) {
   return res.sendFile(path.join(__dirname + '/templateLogReg/index.html'));
 });
+
+router.post('/', User.authentication, User.create);
---
+++ routes/user.js
@@ -0,0 +1,48 @@
+var UserSchema = require('../models/user');
+
+var User = {
+    authentication: function(req, res, next){
+        if (req.body.password !== req.body.passwordConf) {
+            var err = new Error('Passwords do not match.');
+            err.status = 400;
+            res.send("passwords dont match");
+            return next(err);
+          }
+    },
+    create: function(req, res, next){
+        if (req.body.email && req.body.username && req.body.password && req.body.passwordConf) {
+            var userData = {
+              email: req.body.email,
+              username: req.body.username,
+              password: req.body.password,
+            }
+            UserSchema.create(userData, function (error, user) {
+                if (error) {
+                  return next(error);
+                } else {
+                  req.session.userId = user._id;
+                  return res.redirect('/profile');
+                }
+            });          
+        }
+        else if (req.body.logemail && req.body.logpassword) {
+            UserSchema.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+                if (error || !user) {
+                  var err = new Error('Wrong email or password.');
+                  err.status = 401;
+                  return next(err);
+                } else {
+                  req.session.userId = user._id;
+                  return res.redirect('/profile');
+                }
+            });
+        }
+        else {
+            var err = new Error('All fields required.');
+            err.status = 400;
+            return next(err);
+        }
+    }
+}
+
+module.exports = User;
